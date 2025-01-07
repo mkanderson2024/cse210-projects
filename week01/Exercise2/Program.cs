d9c78ef7f7729a0bf236ef9674520b24e3db2bf8
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-07 11:14:51 -0700

    Completed Stretch Challenge

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -7,27 +7,75 @@ class Program
         Console.Write("What is your grade? ");
         string userInput = Console.ReadLine();
         int grade = int.Parse (userInput);
+        string gradeLetter = "";
+        string gradeSign = "";
 
-        if (grade >= 90)
+        if (grade >= 93)
         {
-            Console.WriteLine ("Your grade is: A");
+            gradeLetter = "A";
+            if (grade >= 90 && grade <= 93)
+            {
+                gradeSign = "-";
+            }
+            else
+            {
+                gradeSign = "";
+            }
         }
         else if (grade < 90 && grade >= 80)
         {
-            Console.WriteLine ("Your grade is: B");
+            gradeLetter = "B";
+            if (grade >= 80 && grade <= 83)
+            {
+                gradeSign = "-";
+            }
+            else if (grade >= 87 && grade < 90)
+            {
+                gradeSign = "+";
+            }
+            else
+            {
+                gradeSign = "";
+            }
         }
         else if (grade >= 70 && grade < 80)
         {
-            Console.WriteLine ("Your grade is: C");
+            gradeLetter = "C";
+                       if (grade >= 70 && grade <= 73)
+            {
+                gradeSign = "-";
+            }
+            else if (grade >= 77 && grade < 80)
+            {
+                gradeSign = "+";
+            }
+            else
+            {
+                gradeSign = "";
+            }
         }
         else if (grade >= 60 && grade < 70)
         {
-            Console.WriteLine ("Your grade is: D");
+            gradeLetter = "D";
+                       if (grade >= 60 && grade <= 63)
+            {
+                gradeSign = "-";
+            }
+            else if (grade >= 67 && grade < 70)
+            {
+                gradeSign = "+";
+            }
+            else
+            {
+                gradeSign = "";
+            }
         }
         else 
         {
-            Console.WriteLine ("Your grade is: F");
+            gradeLetter = "F";
         }
+        Console.WriteLine ($"Your grade is {gradeLetter}{gradeSign}");
+
         if (grade >= 70)
         {
             Console.WriteLine ("Congratulations, you're passing!");
