a3593cbcf8eeaef0476e3b38732d0c679982f649
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-07 10:39:13 -0700

    Saving work to current point.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse (userInput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine ("Your grade is: A");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            Console.WriteLine ("Your grade is: B");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.WriteLine ("Your grade is: C");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.WriteLine ("Your grade is: D");
+        }
+        else 
+        {
+            Console.WriteLine ("Your grade is: F");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine ("Congratulations, you're passing!");
+        }
+        else
+        {
+            Console.WriteLine ("You are not passing. Try again, you can do this!");
+        }
     }
 }
