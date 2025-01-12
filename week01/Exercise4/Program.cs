de7214b006a7f8d8362d426bb5970b13b0345cca
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-11 19:33:54 -0700

    Completed Excersize 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int input = -1;
+        List<int> numbers = new List<int>();
+        while (input != 0)
+        {
+            Console.Write("Enter a number. Type 0 when done: ");
+            string responce = Console.ReadLine();
+            input = int.Parse(responce);
+            numbers.Add(input);
+        }
+        int sum = numbers.Sum();
+        Console.WriteLine($"The Sum is: {sum}");
+        double average = Queryable.Average(numbers.AsQueryable());
+        Console.WriteLine($"The Average is: {average}");
+        int maximum = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {maximum}");
     }
 }
