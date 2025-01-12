4fa640ad74bde7a41dc757ab5858f6cfc523c7f3
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-11 18:25:29 -0700

    Completed Excersize 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string replay = "Yes";
+        string answer = "Yes";
+
+        while (replay == answer)
+        {
+            Random randomNumber = new Random();
+            int magicNumber = randomNumber.Next(1, 101);
+            string cont = "yes";
+            string stop = "no";
+            int guessCount = 0;
+
+            while (cont != stop)
+            {
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                int guess = int.Parse(userGuess);
+                guessCount += 1;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                    cont = "yes";
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                    cont = "yes";
+                }
+                else
+                {
+                    Console.WriteLine($"You got it in {guessCount} guesses!");
+                    cont = "no";
+                }
+            }
+
+            Console.Write("Play again? Yes to contine and No to stop: ");
+            answer = Console.ReadLine();
+        }
     }
 }
