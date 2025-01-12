5a9ef7c98d497a612ad0713d6635730527423094
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-11 20:23:06 -0700

    Completed Excersize 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        string namePrint = PromptUserName(name);
+        Console.Write("What is your favorite number? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        int numberOriginal = PromtUserNumber(number);
+        int numberPrint = SquareNumber(numberOriginal);
+        DisplayResult(namePrint, numberPrint);
+    }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName(string userName)
+    {
+        string resultName = userName;
+        return resultName;
+    }
+    static int PromtUserNumber(int numberFavorite)
+    {
+        int resultNumber = numberFavorite;
+        return resultNumber;
+    }
+    static int SquareNumber(int numberFavorite)
+    {
+        int result = numberFavorite * numberFavorite;
+        return result;
+    }
+    static void DisplayResult(string userName, int numberSquared)
+    {
+        Console.WriteLine($"{userName}, your favorite number squared is: {numberSquared}");
     }
 }
